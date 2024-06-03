c329b99bf6d2e9a0e01d45f3c3ea13feb02b9730
Nikos Petrou <79763730+NikosPetrouCh@users.noreply.github.com>
2024-06-03 19:04:54 +0300

    Update Hangman_Game.py

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -1,4 +1,14 @@
-# FOURTH TRY
+# FIFTH TRY
+
+#Notes
+
+# 1. Terminology
+# 2. Consistancy - using Enum 
+# 3. Encapsulation in hangman class. (properties)
+# 4. If statementd are followed by return or continue do not take elif.
+# 5. Input - sanitise -  logic.
+
+
 
 from enum import Enum
 import random
@@ -10,6 +20,7 @@ class GameStatus(Enum):
         LETTER_ALREADY_USED= 4
         WON = 5
         LOST = 6
+        CONTINUE = 7
 
 class Word:
 
@@ -32,81 +43,104 @@ class Word:
 
 class Hangman:
     def __init__(self, word):
-        self.word = word
-        self.players_view = '_' * len(word)
-        self.mistakes = 0
-        self.chosen_letters = []
-        self.Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        self.__word = word
+        self.__players_view = '_' * len(word)
+        self.__mistakes = 0
+        self.__chosen_letters = []
+        
+    @property
+    def word(self):
+        return self.__word
+
+    @property
+    def players_view(self):
+        return self.__players_view
+
+    @property
+    def mistakes(self):
+        return self.__mistakes
+
+    @property
+    def chosen_letters(self):
+        return self.__chosen_letters
+
 
     def check_letter(self, letter):
         if len(letter) != 1 or not letter.isalpha():
             return GameStatus.THIS_IS_NOT_LETTER
-        elif letter in self.chosen_letters:
-            return GameStatus.LETTER_ALREADY_USED
-        self.chosen_letters.append(letter)
         return True
 
     def check_words_letter(self, letter):
-        if letter in self.word:
+        if letter in self.__chosen_letters:
+            return GameStatus.LETTER_ALREADY_USED
+        self.chosen_letters.append(letter)
+
+        if letter in self.__word:
             self.players_view = [letter if letter == self.word[i] else self.players_view[i] for i in range(len(self.word))]
             return GameStatus.LETTER_EXIST
         else:
-            self.mistakes += 1
+            self.__mistakes += 1
             return GameStatus.LETTER_DOESNOT_EXIST
 
     def win_or_loss(self):
-        if self.mistakes >= 6:
+        if self.__mistakes >= 6:
             return GameStatus.LOST
-        elif '_' not in self.players_view:
+        
+        if '_' not in self.__players_view:
             return GameStatus.WON
         else:
-            None
+            return GameStatus.CONTINUE
+
 
 class Game:
     def __init__(self, word):
-        self.game_loop = Hangman(word)
+        self.hangman = Hangman(word)
 
     def play_now(self):
-        while self.game_loop.mistakes < 6 and '_' in self.game_loop.players_view:
+        while self.hangman.__mistakes < 6 and '_' in self.hangman.players_view:
+
             self.display_state()
+
             letter = input("Guess a letter:").lower()
-            letter_check = self.game_loop.check_letter(letter)
-            if  letter_check == GameStatus.LETTER_ALREADY_USED:
+
+            letter_sanitize = self.hangman.check_letter(letter)
+            if  letter_sanitize == GameStatus.THIS_IS_NOT_LETTER:
+                print("This is not a letter, Try again")
+
+            letter_logic = self.hangman.check_words_letter(letter)
+            if letter_logic == GameStatus.LETTER_ALREADY_USED:
                 print("You have already chosen this letter")
-                continue
-            elif not letter_check:
-                continue
-            status = self.game_loop.check_words_letter(letter)
-            if status == GameStatus.LETTER_EXIST:
+
+            if letter_logic == GameStatus.LETTER_EXIST:
                print("Good guess")
-            elif status == GameStatus.LETTER_DOESNOT_EXIST:
+
+            if letter_logic == GameStatus.LETTER_DOESNOT_EXIST:
                print("Wrong guess")
-            self.display_state()
-            result = self.game_loop.win_or_loss()
+
+            result = self.hangman.win_or_loss()
             if result == GameStatus.WON:
-                print("Congratulations!, Yoi found the word")
+                print("Congratulations!, You found the word!")
                 break
-            elif result == GameStatus.LOST:
-                print("You lost! The word was:", self.game_loop.word)
+            if result == GameStatus.LOST:
+                print("You lost! The word was:", self.hangman.word)
                 break
 
     def display_state(self):
         print("Welcome to Hangman!")
-        print(f"Word: {' '.join(self.game_loop.players_view)}")
-        print(f"Mistakes: {self.game_loop.mistakes}")
-        print(f"Chosen letters: {', '.join(self.game_loop.chosen_letters)}")
+        print(f"Word: {' '.join(self.hangman.players_view)}")
+        print(f"Mistakes: {self.hangman.mistakes}")
+        print(f"Chosen letters: {', '.join(self.hangman.chosen_letters)}")
 
     def input(self):
         letter = input("Guess a letter:").lower()
-        if self.game_loop.check_letter(letter):
-            self.game_loop.check_words_letter()
+        if self.hangman.check_letter(letter):
+            self.hangman.check_words_letter()
 
     def mistakes(self):
-        print(f"Wrong guess! Mistakes: {self.game_loop.mistakes}")
+        print(f"Wrong guess! Mistakes: {self.hangman.mistakes}")
 
     def result(self):
-        result = self.game_loop.win_or_loss()
+        result = self.hangman.win_or_loss()
         if result:
             print(result)
 
@@ -115,4 +149,5 @@ class Game:
 word = Word.word_choice()
 game1 = Game(word)
 game1.play_now()
+
 
